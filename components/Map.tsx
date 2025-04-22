c00d4538d2aa0e6edb9d98b7d78c601c4b5b44b6
Sivut Mekareeya <sivut_mek@outlook.com>
2025-04-22 14:37:02 +0700

    init using leaflet map

---
+++ components/Map.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useEffect } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+interface MapProps {
+  position: [number, number];
+}
+
+const Map = ({ position }: MapProps) => {
+  useEffect(() => {
+    // Fix the missing icon issue
+    delete (L.Icon.Default.prototype as any)._getIconUrl;
+    L.Icon.Default.mergeOptions({
+      iconUrl: "/leaflet/marker-icon.png",
+      iconRetinaUrl: "/leaflet/marker-icon-2x.png",
+      shadowUrl: "/leaflet/marker-shadow.png",
+    });
+  }, []);
+
+  return (
+    <MapContainer 
+      center={position} 
+      zoom={13} 
+      scrollWheelZoom={false}
+      style={{ height: "400px", width: "100%" }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default Map; 
