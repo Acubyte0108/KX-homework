14fef76a0471971e25ebaf45cbc98d062200606a
Sivut Mekareeya <sivut_mek@outlook.com>
2025-04-26 16:21:17 +0700

    add draft

--- components/passport-map.tsx
+++ components/passport-map.tsx
@@ -14,7 +14,8 @@ import {
   DrawerTitle,
 } from "@/components/ui/drawer";
 import Image from "next/image";
-import { Skeleton } from "@/components/ui/skeleton";
+import { X } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 const MapWithNoSSR = dynamic(() => import("@/components/map"), {
   ssr: false,
@@ -145,9 +146,9 @@ export function PassportMap({ passport }: PassportMapProps) {
           <Drawer open={true} shouldScaleBackground={false} modal={false}>
             <DrawerContent
               overlayClassName="bg-transparent"
-              className="bg-gray-900/20 backdrop-blur-md flex flex-col"
+              className="bg-gray-900/20 backdrop-blur-lg flex flex-col"
             >
-              <DrawerHeader className="text-center">
+              {/* <DrawerHeader className="text-center">
                 <DrawerTitle className="text-white">
                   Tab the slot or location pin to information
                 </DrawerTitle>
@@ -174,6 +175,52 @@ export function PassportMap({ passport }: PassportMapProps) {
                     />
                   </div>
                 ))}
+              </div> */}
+              <div className="flex flex-col gap-4 p-4">
+                <div className="flex justify-center items-center gap-6">
+                  <div className="flex items-center justify-center">
+                    <div className="rounded-full bg-slate-800 p-2 mt-4">
+                      {selectedEvent && (
+                        <Image
+                          src={selectedEvent.image_url}
+                          alt={`Event ${selectedEvent.id}`}
+                          width={128}
+                          height={128}
+                          className="rounded-full"
+                          onError={(e) => {
+                            const target = e.target as HTMLImageElement;
+                            target.src = `/placeholder.jpg`;
+                          }}
+                        />
+                      )}
+                    </div>
+                  </div>
+                  <div className="flex flex-col gap-2 text-white">
+                    <h1 className="text-lg text-left font-semibold">
+                      {`ทางม้าลายแยกเฉลิมบุรี (${selectedEvent?.id})`}
+                    </h1>
+                    <div className="text-left text-sm">
+                      Available to collect from 4 Dec 2024 00:00 to 31 Dec 2025
+                      23:59
+                    </div>
+                  </div>
+                  <Button
+                    onClick={() => setSelectedEvent(null)}
+                    className="rounded-full bg-white/20 cursor-pointer text-white size-6 self-start"
+                    variant="ghost"
+                    size="icon"
+                    aria-label="Close"
+                  >
+                    <X className="h-6 w-6" />
+                  </Button>
+                </div>
+                <Button
+                  className="w-full mb-2 bg-coral-pink hover:bg-coral-pink/80 rounded-full cursor-pointer"
+                  variant="default"
+                  size="lg"
+                >
+                  Collect Now
+                </Button>
               </div>
             </DrawerContent>
           </Drawer>
